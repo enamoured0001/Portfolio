378a22b3c40615311852f85cf2a6a4d81065f2c0
Mohit <mohitpandey9315339061@gmail.com>
2025-02-16 07:43:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Home from './components/home'
+import Home from './components/Home'
 import Education from './components/education'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
